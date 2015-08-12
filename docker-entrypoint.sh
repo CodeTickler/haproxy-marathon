c7169d50137992977d2b826c400c1f2ed0e8755c
Volodymyr Kuznetsov <ks.vladimir@gmail.com>
2015-08-12 14:43:52 +0200

    Run iptables cleanup command in a loop to delete duplicate rules

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -29,8 +29,8 @@ function haproxy_soft_cleanup {
   log "Cleaning up iptables rules for ports ${PORTS}"
 
   for PORT in $PORTS; do
-      echo "Deleting iptables rule for port ${PORT}; please ignore 'Bad rule' error if it appears below"
-      iptables -D INPUT -p tcp --dport $PORT --syn -j DROP || true
+      >&2 echo "Deleting iptables rule for port ${PORT}; please ignore 'Bad rule' error if it appears below"
+      while iptables -D INPUT -p tcp --dport $PORT --syn -j DROP; do true; done
   done
 }
 
